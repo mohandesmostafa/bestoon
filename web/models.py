06587120cf77ee145ad51233da5186e8df0de638
mahi <m_mahheidari@yahoo.com>
2018-04-22 19:14:46 +0430

    income in models and som beatification

--- web/models.py
+++ web/models.py
@@ -7,6 +7,21 @@ from django.contrib.auth.models import User
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
-    amunt = models.BigIntegerField()
+    amount = models.BigIntegerField()
     #kharj ro che kasi anjam dade
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __unicode__(self):
+#        return self.text
+        return "{}-{}".format(self.date, self.amount)
+
+#sod bank:
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    # kharj ro che kasi anjam dade
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __unicode__(self):
+        #        return self.text
+        return "{}-{}".format(self.date, self.amount)
+
